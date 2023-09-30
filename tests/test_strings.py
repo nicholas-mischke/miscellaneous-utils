8f30df7c8929c77886ed1196535d8f4be137cfc2
Nicholas Mischke <nmischkework@proton.me>
2023-09-29 21:19:53 -0500

    Added CaseConverter class

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,5 +1,9 @@
 import pytest
-from miscellaneous_utilities.strings import normalize_space, normalize_newlines
+from miscellaneous_utilities.strings import (
+    normalize_space,
+    normalize_newlines,
+    CaseConverter,
+)
 
 
 @pytest.mark.parametrize(
@@ -16,16 +20,78 @@ from miscellaneous_utilities.strings import normalize_space, normalize_newlines
 def test_normalize_space(input_string, expected):
     assert normalize_space(input_string) == expected
 
+
 @pytest.mark.parametrize(
     "input_string, leading_newline, trailing_newline, expected",
     [
-        ("\n\n\nmultiple\n\n\n\n\nspaces\n\n\nbetween\n\n\nwords\n\n\n", False, False, "multiple\n\nspaces\n\nbetween\n\nwords"),
-        ("\n\n\nmultiple\n\n\n\n\nspaces\n\n\nbetween\n\n\nwords\n\n\n", True, False, "\nmultiple\n\nspaces\n\nbetween\n\nwords"),
-        ("\n\n\nmultiple\n\n\n\n\nspaces\n\n\nbetween\n\n\nwords\n\n\n", False, True, "multiple\n\nspaces\n\nbetween\n\nwords\n"),
-    ]
+        (
+            "\n\n\nmultiple\n\n\n\n\nspaces\n\n\nbetween\n\n\nwords\n\n\n",
+            False,
+            False,
+            "multiple\n\nspaces\n\nbetween\n\nwords",
+        ),
+        (
+            "\n\n\nmultiple\n\n\n\n\nspaces\n\n\nbetween\n\n\nwords\n\n\n",
+            True,
+            False,
+            "\nmultiple\n\nspaces\n\nbetween\n\nwords",
+        ),
+        (
+            "\n\n\nmultiple\n\n\n\n\nspaces\n\n\nbetween\n\n\nwords\n\n\n",
+            False,
+            True,
+            "multiple\n\nspaces\n\nbetween\n\nwords\n",
+        ),
+    ],
 )
-def test_normalize_newlines(input_string: str, leading_newline: bool, trailing_newline: bool, expected: str):
-    assert normalize_newlines(input_string, leading_newline, trailing_newline) == expected
+def test_normalize_newlines(
+    input_string: str, leading_newline: bool, trailing_newline: bool, expected: str
+):
+    assert (
+        normalize_newlines(input_string, leading_newline, trailing_newline) == expected
+    )
+
+
+@pytest.mark.parametrize(
+    "input_string",
+    [
+        "hello_world",
+        "HelloWorld",
+        "helloWorld",
+        "hello-world",
+    ],
+)
+def test_case_converter(input_string):
+    converter = CaseConverter(input_string)
+    assert converter.snake_case == "hello_world"
+    assert converter.pascal_case == "HelloWorld"
+    assert converter.camel_case == "helloWorld"
+    assert converter.kebab_case == "hello-world"
+
+
+def test_case_converter_class_names():
+    converter = CaseConverter("ThisIsANameOfAClass")
+    assert converter.snake_case == "this_is_a_name_of_a_class"
+    assert converter.pascal_case == "ThisIsANameOfAClass"
+    assert converter.camel_case == "thisIsANameOfAClass"
+    assert converter.kebab_case == "this-is-a-name-of-a-class"
+    assert converter.word_list == [
+        "this",
+        "is",
+        "a",
+        "name",
+        "of",
+        "a",
+        "class",
+    ]
+
+def test_case_converter_error():
+    with pytest.raises(ValueError):
+        CaseConverter("")
+
+    with pytest.raises(ValueError):
+        CaseConverter("Hello, Wolrd!")
+
 
 if __name__ == "__main__":
-    pytest.main(["-k", "test_normalize"])
+    pytest.main(["-k", "test_case_converter"])
